7e0889682773ff331ca7a041624f60ad67600c1f
joaobranco20 <joaobugarimbranco@gmail.com>
2022-04-27 11:52:16 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { Header } from './components/Header';
+import { Country } from "./pages/Country";
+import { Home } from "./pages/Home";
+import './styles/global.scss'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route index path="/" element={<Home />}/>
+            <Route path="country/:cca3" element={<Country />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
+    
+  );
+}
+
+export default App;
